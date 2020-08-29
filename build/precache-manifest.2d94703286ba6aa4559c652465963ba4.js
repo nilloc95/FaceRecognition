dcce9040cffbb529f181e00df158163b40b3acf8
Collin Gilmore <collingilmore@gmail.com>
2020-08-28 20:23:19 -0400

    added npm serve to build properly for production

---
+++ build/precache-manifest.2d94703286ba6aa4559c652465963ba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1938e5a8b464f8c3f3318ab15308e551",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "c8b8e208985329147c21",
+    "url": "/FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d161b0fa98a63d2cbe0c",
+    "url": "/FaceRecognition/static/css/main.2de8ffa0.chunk.css"
+  },
+  {
+    "revision": "c8b8e208985329147c21",
+    "url": "/FaceRecognition/static/js/2.4da6a651.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition/static/js/2.4da6a651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d161b0fa98a63d2cbe0c",
+    "url": "/FaceRecognition/static/js/main.0342a3c3.chunk.js"
+  },
+  {
+    "revision": "7d6d344d67725b11bfb5",
+    "url": "/FaceRecognition/static/js/runtime-main.3a0102b3.js"
+  }
+]);
